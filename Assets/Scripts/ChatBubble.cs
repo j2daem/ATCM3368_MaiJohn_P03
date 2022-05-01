4ff4142de8259ec006b55c6e824ffa0af99aaea8
John Mai <johnhuymai@gmail.com>
2022-05-01 06:00:09 -0500

    Set-up project

---
+++ Assets/Scripts/ChatBubble.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public enum direction { Left, Right };
+
+[CreateAssetMenu(fileName = "NewChatBubble", menuName = "Chat Bubble")]
+public class ChatBubble : ScriptableObject
+{
+    
+
+    [SerializeField] string Text;
+    [SerializeField] int fontSize = 80;
+    [SerializeField] float Duration;
+    [SerializeField] bool Shake;
+
+    [SerializeField] direction TailDirection;
+    [SerializeField] Color Color = Color.black;
+
+    [SerializeField] Sprite Shape;
+    [SerializeField] AudioClip SFX;
+    [SerializeField] TMP_FontAsset Font;
+    
+
+    /* Getters */
+    public string GetText => Text;
+    public int GetFontSize => fontSize;
+    public float GetDuration => Duration;
+    public bool GetShake => Shake;
+    public direction GetDirection => TailDirection;
+    public Color GetColor => Color;
+    public Sprite GetShape => Shape;
+    public AudioClip GetSFX => SFX;
+    public TMP_FontAsset GetFont => Font;
+}
---
+++ Assets/Scripts/ChatBubbleDisplay.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class ChatBubbleDisplay : MonoBehaviour
+{
+    [SerializeField] ChatBubble chatBubble;
+
+    [SerializeField] TextMeshProUGUI bubbleText;
+    [SerializeField] Image bubbleShape;
+    [SerializeField] AudioSource bubbleSFX;
+    [SerializeField] Animator bubbleAnimator;
+
+    void Start()
+    {
+        /* Text properties */
+        bubbleText.text = chatBubble.GetText;
+        bubbleText.font = chatBubble.GetFont;
+        bubbleText.fontSize = chatBubble.GetFontSize;
+        bubbleText.color = chatBubble.GetColor;
+
+        bubbleShape.sprite = chatBubble.GetShape;
+        bubbleSFX.clip = chatBubble.GetSFX;
+
+        StartCoroutine(PlayAnimation());
+        bubbleSFX.Play();
+    }
+
+    IEnumerator PlayAnimation()
+    {
+        
+        if (chatBubble.GetShake)
+        {
+            bubbleAnimator.SetBool("shake", true);
+        }
+
+        yield return new WaitForSeconds(chatBubble.GetDuration);
+
+        bubbleAnimator.SetBool("completed", true);
+    }
+}
